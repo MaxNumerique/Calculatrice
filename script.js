d868ff371ede4bddf3c8b0c1a7c9f83e7204758f
MaxNumerique <maximilienlandois@gmail.com>
2024-03-11 16:16:52 +0100

    de

---
+++ script.js
@@ -0,0 +1,25 @@
+//====================================================================================
+//====================================================================================
+//====================================================================================
+
+let input = "";
+let ecran = document.getElementsByClassName("résultat");
+
+function ajouterEcran() {
+    ecran = value.input;
+}
+
+function updateScreen () {
+    document.getElementsByClassName('résultat').value = input;
+}
+
+function inputChiffre (number) {
+    input += number;
+}
+
+function inputOperation(operator) {
+    input += operator;
+    
+}
+
+
